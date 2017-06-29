1a542843706a9e6144ed22510f3ece7ffd6d9a9b
Nate Lee <nate.lee@logicsolutions.com>
2017-06-29 17:02:45 +0800

    Finish navigation for mobile device

--- 0/3/js/main.js
+++ 0/3/js/main.js
@@ -1,1 +1,14 @@
+jQuery(document).ready(function($) {
+	$('#btn-nav-mobile').on('click', showMobileNavbar);
+	$('#btn-close-nav, #overlay').on('click', hideMobileNavbar);
+});
 
+function showMobileNavbar() {
+	$('#nav--mobile').fadeIn(400);
+	$('#overlay').fadeIn(400);
+}
+
+function hideMobileNavbar() {
+	$('#nav--mobile').fadeOut(400);
+	$('#overlay').fadeOut(400);
+}
